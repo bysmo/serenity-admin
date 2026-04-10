c08c69b7c495ebc71a075a791baff2f05f341fcd
modestemariebegninesomda <modestesomda@gmail.com>
2026-04-10 00:05:45 +0000

    ajout du trait hasApiTokens

--- app/Models/Membre.php
+++ app/Models/Membre.php
@@ -6,12 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 use App\Traits\HasChecksum;
 
 class Membre extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable, HasChecksum;
+    use HasFactory, Notifiable, HasChecksum, HasApiTokens;
 
     protected $fillable = [
         'numero',
